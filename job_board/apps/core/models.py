7faadce1f8ffd130f14a40da0a8c17aac7928b0c
Saqif Haque <saqifhaque@gmail.com>
2021-01-23 19:18:19 +0600

    restapi setup and super user created

--- job_board/apps/core/models.py
+++ job_board/apps/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class jobs (models.Model):
+    job_title = models.CharField(max_length=250)
+    location = models.TextField();
+
+    def __str__(self):
+        return self.job_title
